a5f60274e4d4d54ad61b17935de47d25ac984c89
Tony Miller <mcfiredrill@gmail.com>
2010-11-22 20:17:45 -0800

    Multiple stylesheets, selectable with javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function set_stylesheet(styletitle){
+	var links=document.getElementsByTagName("link");
+	var found=false;
+	for(var i=0;i<links.length;i++)
+	{
+		var rel=links[i].getAttribute("rel");
+		var title=links[i].getAttribute("title");
+		if(rel.indexOf("style")!=-1&&title)
+		{
+			links[i].disabled=true; // IE needs this to work. IE needs to die.
+			if(styletitle==title) { links[i].disabled=false; found=true; }
+		}
+	}
+	//if(!found) set_preferred_stylesheet();
+}

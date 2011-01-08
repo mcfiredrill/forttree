4cad9f6300e66c0c4d986bb75f4501dc6f84ad97
Tony Miller <mcfiredrill@gmail.com>
2011-01-08 14:59:30 -0800

    Moved themes to themes/ directory.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,17 @@ module ApplicationHelper
     "(づ｡◕‿‿‿‿◕｡)づ",
     "♡✰☁☼✿⚈‿‿⚈✿☼☁✰♡"]
   end
+  
+  def themes
+    Dir['public/stylesheets/themes/*.css'] 
+  end
+
+  def theme_basenames
+    names = Array.new
+    themes.each do |t|
+      names << t[/(\w*)\.css/, 1]
+    end
+    return names
+  end
+
 end

9400abd56519d83ca6018972362b1b3e170870a6
Tony Miller <mcfiredrill@gmail.com>
2011-04-06 09:30:43 -0700

    Make smiley assistant configurable.

--- fortconfig_defaults.rb
+++ fortconfig_defaults.rb
@@ -1,3 +1,4 @@
 eval "BOARDTITLE = 'FortTree'" unless defined? BOARDTITLE
 eval "BRANCHES_PER_PAGE = 10" unless defined? BRANCHES_PER_PAGE
 eval "SHOWTITLETXT=true" unless defined? SHOWTITLETXT
+eval "SMILEY_ASSIST=true" unless defined? SMILEY_ASSIST

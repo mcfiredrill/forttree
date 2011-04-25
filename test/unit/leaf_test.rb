60d09f4ee25eb60a13fc7235d1c10ee15e2afc73
Tony Miller <mcfiredrill@gmail.com>
2011-04-25 14:10:37 -0700

    Unit test for valid leaf.

--- test/unit/leaf_test.rb
+++ test/unit/leaf_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class LeafTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "valid leaf" do
+    leaf = Leaf.new
+    assert !leaf.valid?
+    leaf.content = "Chillbert's vacation."
+    assert leaf.valid?
+    leaf.content = ""
+    leaf.photo_file_name = "chillbert.jpg"
+    assert leaf.valid?
   end
 end

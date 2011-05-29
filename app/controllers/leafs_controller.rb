3c0e826d81d8e76c2c753fbd69cebc28e670e1c1
Tony Miller <mcfiredrill@gmail.com>
2011-05-29 09:52:34 -0700

    Clean up controllers.

--- app/controllers/leafs_controller.rb
+++ app/controllers/leafs_controller.rb
@@ -1,7 +1,5 @@
 class LeafsController < ApplicationController
 
-  # GET /leafs/new
-  # GET /leafs/new.xml
   def new
     @leaf = Leaf.new
     @branch = Branch.find(params[:branch])
@@ -9,26 +7,11 @@ class LeafsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @leaf }
     end
   end
 
-  # GET /leafs/1/edit
-  #def edit
-  #  @leaf = Leaf.find(params[:id])
-  #end
-
-  # POST /leafs
-  # POST /leafs.xml
   def create
-    logger.info("the id: #{$theid}")
-    logger.info("branch: #{@branch}")
-    logger.info("params hash: #{ params }")
-    logger.info("request_uri: #{ request.request_uri }")
     @parentbranch = Branch.find($theid)
-    #@parentbranch = Branch.find(params[:branch])
-    #@parentbranch = Branch.find(params[:leaf][:branch_id])
-    #@leaf = Leaf.new(:content => params[:leaf][:content], :branch_id => @parentbranch.id, :name => params[:leaf][:name], :photo => params[:leaf][:photo])
     @leaf = Leaf.new(params[:leaf])
     @leaf.branch_id = @parentbranch.id
 
@@ -45,29 +28,10 @@ class LeafsController < ApplicationController
       else
         @branch = Branch.find($theid)
         format.html { render :action => "new" }
-        logger.info("reply, leaf save failed: #{ @leaf.errors }")
       end
     end
   end
 
-  # PUT /leafs/1
-  # PUT /leafs/1.xml
-  #def update
-  #  @leaf = Leaf.find(params[:id])
-
-  #  respond_to do |format|
-  #    if @leaf.update_attributes(params[:leaf])
-  #      format.html { redirect_to(@leaf, :notice => 'Leaf was successfully updated.') }
-  #      format.xml  { head :ok }
-  #    else
-  #      format.html { render :action => "edit" }
-  #      format.xml  { render :xml => @leaf.errors, :status => :unprocessable_entity }
-  #    end
-  #  end
-  #end
-
-  # DELETE /leafs/1
-  # DELETE /leafs/1.xml
   def destroy
     @leaf = Leaf.find(params[:id])
     @branch = Branch.find(@leaf.branch_id)
@@ -83,6 +47,5 @@ class LeafsController < ApplicationController
           format.html { redirect_to('/branches#index') }
       end
     end
-
   end
 end

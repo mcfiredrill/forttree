a30a60abb03e203eefe24b2df0aa5ed512140a15
Nik Nyby <niknyby@gmail.com>
2011-09-17 02:01:44 -0700

    svg fix: allow svg uploads, and don't show thumbnailed svg cause paperclip won't do that
    Conflicts:
    
    	fortconfig_defaults.rb

--- fortconfig_defaults.rb
+++ fortconfig_defaults.rb
@@ -3,4 +3,4 @@ eval "BRANCHES_PER_PAGE = 10" unless defined? BRANCHES_PER_PAGE
 eval "SHOWTITLETXT=true" unless defined? SHOWTITLETXT
 eval "SMILEY_ASSIST=true" unless defined? SMILEY_ASSIST
 eval "MAX_UPLOAD_SIZE = 5.megabytes" unless defined? MAX_UPLOAD_SIZE
-eval "VALID_ATTACHMENT_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/pjpeg', 'image/tiff', 'image/x-png', 'image/bmp', 'image/svg-xml']" unless defined? VALID_ATTACHMENT_TYPES
+eval "VALID_ATTACHMENT_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/pjpeg', 'image/tiff', 'image/x-png', 'image/bmp', 'image/svg+xml']" unless defined? VALID_ATTACHMENT_TYPES

2f5973c7555c95b7a53c6f9b71e30d836d87eae3
Tony Miller <mcfiredrill@gmail.com>
2011-11-16 18:25:22 -0800

    branches controller test

--- test/functional/branches_controller_test.rb
+++ test/functional/branches_controller_test.rb
@@ -1,49 +1,31 @@
 require 'test_helper'
 
 class BranchesControllerTest < ActionController::TestCase
-  setup do
-    @branch = branches(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:branches)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create branch" do
-    assert_difference('Branch.count') do
-      post :create, :branch => @branch.attributes
+  context "a branches controller" do
+    setup do
+      @branch = Factory.create :branch
     end
 
-    assert_redirected_to branch_path(assigns(:branch))
-  end
-
-  test "should show branch" do
-    get :show, :id => @branch.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @branch.to_param
-    assert_response :success
-  end
-
-  test "should update branch" do
-    put :update, :id => @branch.to_param, :branch => @branch.attributes
-    assert_redirected_to branch_path(assigns(:branch))
-  end
-
-  test "should destroy branch" do
-    assert_difference('Branch.count', -1) do
-      delete :destroy, :id => @branch.to_param
+    should "get new" do
+      get :new
+      assert_response :success
     end
 
-    assert_redirected_to branches_path
+    should "create branch" do
+      assert_difference('Branch.count', 1) do
+        post :create, :leaf => {:content => @branch.leafs.first.content}
+      end
+
+      assert_redirected_to new_branch_path
+    end
+
+    should "destroy branch" do
+      @admin = Factory.create :admin
+      assert_difference('Branch.count', -1) do
+        delete :destroy, :delete => [@branch.leafs.first.id], :password => @admin.password, :id => "new"
+      end
+
+      assert_redirected_to new_branch_path
+    end
   end
 end

f8e016231cb7738b1e2f14c1aefa7535a286fbdf
Tony Miller <mcfiredrill@gmail.com>
2011-11-16 18:31:28 -0800

    leafs controller test

--- test/functional/leafs_controller_test.rb
+++ test/functional/leafs_controller_test.rb
@@ -1,49 +1,23 @@
 require 'test_helper'
 
 class LeafsControllerTest < ActionController::TestCase
-  setup do
-    @leaf = leafs(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:leafs)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create leaf" do
-    assert_difference('Leaf.count') do
-      post :create, :leaf => @leaf.attributes
+  context "a leafs controller" do
+    setup do
+      @branch = Factory.create :branch
+      @leaf = @branch.leafs.first
     end
 
-    assert_redirected_to leaf_path(assigns(:leaf))
-  end
-
-  test "should show leaf" do
-    get :show, :id => @leaf.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @leaf.to_param
-    assert_response :success
-  end
-
-  test "should update leaf" do
-    put :update, :id => @leaf.to_param, :leaf => @leaf.attributes
-    assert_redirected_to leaf_path(assigns(:leaf))
-  end
-
-  test "should destroy leaf" do
-    assert_difference('Leaf.count', -1) do
-      delete :destroy, :id => @leaf.to_param
+    should "get new" do
+      get :new, :id => @branch.id
+      assert_response :success
     end
 
-    assert_redirected_to leafs_path
+    should "create leaf" do
+      assert_difference('Leaf.count') do
+        post :create, :branch_id => @branch.id, :leaf => @leaf.attributes
+      end
+
+      assert_redirected_to new_branch_path
+    end
   end
 end

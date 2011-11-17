a1f706f0024fc0e11e857bbd0d0020e9606b7c02
Tony Miller <mcfiredrill@gmail.com>
2011-11-16 20:24:51 -0800

    Add fortconfig files again...

---
+++ fortconfig.rb
@@ -0,0 +1,24 @@
+#
+#You must specify ADMIN_PASS and STORAGE, at the very least.
+
+#System config
+#ADMIN_PASS = #used for editing and deleting posts
+
+#STORAGE = 's3' #storage for image, valid values: 'local', 's3'
+#S3_BUCKET ='' #set these up if you are using s3 for storage, otherwise ignore
+#S3_KEY =''
+#S3_SECRET =''
+#PHOTO_PATH = ':rails_root/public:url'
+
+#style
+#BOARDTITLE='FortTree' #name of this image board
+#SHOWTITLETXT=true #display BOARDTITLE at top of page
+#BRANCHES_PER_PAGE = 10 #threads per page shown
+#LEAFS_PER_BRANCH = 10 #replies per thread shown
+
+#limitations
+#MAX_UPLOAD_SIZE = 5.megabytes #max upload size in KB for images
+#VALID_ATTACHMENT_TYPES = ['image/jpg', 'image/png', 'image/gif', 'image/pjpeg', 'image/tiff', 'image/x-png', 'image/bmp', 'image/svg-xml'] #valid image MIME types, all others will be rejected
+
+#misc
+#SMILEY_ASSIST = true #boolean, turns smiley assistant on or off

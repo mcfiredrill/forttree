a5d19a3c273766d2fdb2a1c798e1a8445e7df8cd
Nik Nyby <niknyby@gmail.com>
2011-12-16 03:21:46 -0800

    unobtrusively added environment variables so i can deploy to heroku again

--- app/helpers/fortconfig_defaults.rb
+++ app/helpers/fortconfig_defaults.rb
@@ -5,7 +5,12 @@ SMILEY_ASSIST=true
 MAX_UPLOAD_SIZE = 5.megabytes
 VALID_ATTACHMENT_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/pjpeg', 'image/tiff', 'image/x-png', 'image/bmp', 'image/svg+xml']
 STORAGE = :filesystem
+PHOTO_PATH = ':rails_root/public:url'
+
+# okay tony if you don't use environment variables this won't affect you
+# anymore
 S3_BUCKET = ENV['S3_BUCKET']
 S3_KEY = ENV['S3_KEY']
 S3_SECRET = ENV['S3_SECRET']
-PHOTO_PATH = ':rails_root/public:url'
+ENV['STORAGE'] and STORAGE = ENV['STORAGE']
+ENV['PHOTO_PATH'] and PHOTO_PATH = ENV['PHOTO_PATH']

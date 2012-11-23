22932529bf7c0d3c1bdc910d6f379e5ce71f089f
Tony Miller <mcfiredrill@gmail.com>
2012-11-23 12:05:40 -0800

    remove fortconfig require from test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require './test/factories'
-require 'fortconfig_defaults.rb' #test with default configuration
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods

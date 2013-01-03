0349cedeabb5a669d12d58dfa659bf479d492a79
Tony Miller <mcfiredrill@gmail.com>
2013-01-02 17:14:16 -0800

    reply to threads spec

---
+++ spec/requests/reply_to_threads_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "A visitor" do
+  before(:each) do
+    @branch = create :branch
+  end
+  it "replies to a post" do
+    name = "anon"
+    content = "hi im replying to a post"
+    image = "#{::Rails.root.to_s}/spec/fixtures/forttree.png"
+
+    visit "/"
+    click_link "Reply"
+    fill_in "leaf[name]", with: name
+    fill_in "leaf[content]", with: content
+    attach_file "leaf[photo]", image
+    click_button 'Post'
+    page.should have_content 'Leaf created!'
+    page.should have_content name
+    page.should have_content content
+    page.should have_xpath "//img[contains(@src,\"#{File.basename(image)}\")]"
+  end
+end

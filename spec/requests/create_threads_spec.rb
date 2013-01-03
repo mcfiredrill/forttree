cb04c6f55230781f22d99710a4ed51e230ca5bee
Tony Miller <mcfiredrill@gmail.com>
2013-01-02 17:04:26 -0800

    spec for creating posts

---
+++ spec/requests/create_threads_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "A visitor" do
+  it "creates a new post" do
+    name = "anon"
+    content = "hi im making a post"
+    image = "#{::Rails.root.to_s}/spec/fixtures/forttree.png"
+
+    visit new_branch_path
+    fill_in "leaf[name]", with: name
+    fill_in "leaf[content]", with: content
+    attach_file "leaf[photo]", image
+    click_button 'Post'
+    page.should have_content 'Branch created!'
+    page.should have_content 'anon'
+    page.should have_content 'hi im making a post'
+    page.should have_xpath "//img[contains(@src,\"#{File.basename(image)}\")]"
+  end
+end

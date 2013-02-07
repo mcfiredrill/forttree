c456fb23683f7d049107d1fc7ac8c95d20637d3d
Tony Miller <mcfiredrill@gmail.com>
2013-02-07 15:22:11 -0800

    add capybara to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Capybara::DSL
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:

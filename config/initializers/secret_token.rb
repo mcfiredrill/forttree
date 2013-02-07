7c647dd42d0c2b4f40946697fbfdef9c0f8719e4
Tony Miller <mcfiredrill@gmail.com>
2013-02-06 16:48:07 -0800

    put secret token in env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Forttree::Application.config.secret_token = '9f9e9cf942d5ab79bfc2326d6aca849e9f29791e74cf621e97a9911d9bb186ee3efc5147dc62bd9e27e7978bc32545489d555e6cf0cf7bc4cd94b595b5e9f4be'
+Forttree::Application.config.secret_token = ENV['SECRET_TOKEN']

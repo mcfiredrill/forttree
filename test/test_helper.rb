4548b73e949766e42319af4e7b18e8d522a14cb0
Tony Miller <mcfiredrill@gmail.com>
2013-03-09 22:38:03 -0800

    delete test

--- test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-require './test/factories'
-
-class ActiveSupport::TestCase
-  include FactoryGirl::Syntax::Methods
-end
--- test/unit/branch_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class BranchTest < ActiveSupport::TestCase
-  should have_many :leafs
-  context "a branch" do
-    setup { @branch = create(:branch) }
-  end
-end

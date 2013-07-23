aae48aeb74c20e541dbea08b8f6e51cc402c9318
Tony Miller <mcfiredrill@gmail.com>
2013-07-23 11:26:22 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,24 +9,24 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110425114446) do
+ActiveRecord::Schema.define(version: 20110425114446) do
 
-  create_table "admins", :force => true do |t|
+  create_table "admins", force: true do |t|
     t.string   "password_hash"
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "branches", :force => true do |t|
+  create_table "branches", force: true do |t|
     t.datetime "last_post_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "leafs", :force => true do |t|
+  create_table "leafs", force: true do |t|
     t.string   "name"
     t.text     "content"
     t.datetime "created_at"

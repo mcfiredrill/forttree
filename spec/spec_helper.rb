3ab85ce5cd2bdc0f14b14b005b80a83334703d4f
Tony Miller <mcfiredrill@gmail.com>
2013-07-23 11:13:22 -0700

    unneeded requires in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'capybara/rspec'
-require './spec/factories'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

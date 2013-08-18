5afdf86f24932795296095d3d5abac074b75fbd8
Tony Miller <mcfiredrill@gmail.com>
2013-08-17 21:25:30 -0700

    add s3 creds to board banners

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,11 @@
 class Board < ActiveRecord::Base
   has_many :branches
-  has_attached_file :banner
+  has_attached_file :banner,
+      :storage => STORAGE,
+      :path => PHOTO_PATH,
+      :s3_credentials => {
+        :access_key_id => S3_KEY,
+        :secret_access_key => S3_SECRET
+      },
+      :bucket => S3_BUCKET
 end

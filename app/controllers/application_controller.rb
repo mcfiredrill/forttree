37a1ff79114efdd2184bb42695646eb08a0c3a17
Tony Miller <mcfiredrill@gmail.com>
2013-12-03 21:11:14 +0900

    mobile stylesheet, use prepare_for_mobile before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :prepare_for_mobile
 
-  def check_for_mobile
-    session[:mobile_override] = params[:mobile] if params[:mobile]
-  end
+  private
 
   def mobile_device?
     if session[:mobile_override]
       session[:mobile_override] == "1"
     else
-      # Season this regexp to taste. I prefer to treat iPad as non-mobile.
       (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
     end
   end
   helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+  end
 end

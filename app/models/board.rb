0c99ab11c4bf7d38d15ea74622f1f92cb55e99ea
Tony Miller <mcfiredrill@gmail.com>
2014-07-14 17:08:13 +0900

    add required validator for board attachment

--- app/models/board.rb
+++ app/models/board.rb
@@ -9,4 +9,6 @@ class Board < ActiveRecord::Base
       },
       :bucket => S3_BUCKET
   default_scope -> { order('name ASC') }
+  validates_attachment_content_type :banner, :content_type => VALID_ATTACHMENT_TYPES, :if => Proc.new { |p| p.errors.blank? }
+  validates_attachment_size :banner, :less_than => MAX_UPLOAD_SIZE, :message => "File too big! :{", :if => Proc.new { |p| p.errors.blank? }
 end

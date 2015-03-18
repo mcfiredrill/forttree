10347d05708ede9634949a7d43aadb67515338b1
Tony Miller <mcfiredrill@gmail.com>
2015-03-18 22:36:57 +0900

    move sendgrid settings to environments/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,19 @@ Forttree::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
+  config.action_mailer.default_url_options = { :host => "fortchan" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable threaded mode
   # config.threadsafe!

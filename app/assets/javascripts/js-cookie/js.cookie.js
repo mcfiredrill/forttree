8d0301ee7ba9f9993ab96708c20b08f6a1ebc52b
Nik Nyby <nnyby@columbia.edu>
2017-01-12 09:40:31 -0500

    :arrow_up: js-cookie 2.1.3

--- app/assets/javascripts/js-cookie/js.cookie.js
+++ app/assets/javascripts/js-cookie/js.cookie.js
@@ -1,20 +1,25 @@
 /*!
- * JavaScript Cookie v2.0.4
+ * JavaScript Cookie v2.1.3
  * https://github.com/js-cookie/js-cookie
  *
  * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
  * Released under the MIT license
  */
-(function (factory) {
+;(function (factory) {
+	var registeredInModuleLoader = false;
 	if (typeof define === 'function' && define.amd) {
 		define(factory);
-	} else if (typeof exports === 'object') {
+		registeredInModuleLoader = true;
+	}
+	if (typeof exports === 'object') {
 		module.exports = factory();
-	} else {
-		var _OldCookies = window.Cookies;
+		registeredInModuleLoader = true;
+	}
+	if (!registeredInModuleLoader) {
+		var OldCookies = window.Cookies;
 		var api = window.Cookies = factory();
 		api.noConflict = function () {
-			window.Cookies = _OldCookies;
+			window.Cookies = OldCookies;
 			return api;
 		};
 	}
@@ -34,6 +39,9 @@
 	function init (converter) {
 		function api (key, value, attributes) {
 			var result;
+			if (typeof document === 'undefined') {
+				return;
+			}
 
 			// Write
 
@@ -68,9 +76,9 @@
 
 				return (document.cookie = [
 					key, '=', value,
-					attributes.expires && '; expires=' + attributes.expires.toUTCString(), // use expires attribute, max-age is not supported by IE
-					attributes.path    && '; path=' + attributes.path,
-					attributes.domain  && '; domain=' + attributes.domain,
+					attributes.expires ? '; expires=' + attributes.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+					attributes.path ? '; path=' + attributes.path : '',
+					attributes.domain ? '; domain=' + attributes.domain : '',
 					attributes.secure ? '; secure' : ''
 				].join(''));
 			}
@@ -90,7 +98,6 @@
 
 			for (; i < cookies.length; i++) {
 				var parts = cookies[i].split('=');
-				var name = parts[0].replace(rdecode, decodeURIComponent);
 				var cookie = parts.slice(1).join('=');
 
 				if (cookie.charAt(0) === '"') {
@@ -98,6 +105,7 @@
 				}
 
 				try {
+					var name = parts[0].replace(rdecode, decodeURIComponent);
 					cookie = converter.read ?
 						converter.read(cookie, name) : converter(cookie, name) ||
 						cookie.replace(rdecode, decodeURIComponent);
@@ -122,7 +130,10 @@
 			return result;
 		}
 
-		api.get = api.set = api;
+		api.set = api;
+		api.get = function (key) {
+			return api.call(api, key);
+		};
 		api.getJSON = function () {
 			return api.apply({
 				json: true

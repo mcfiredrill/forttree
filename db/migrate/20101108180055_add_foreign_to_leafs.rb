371cb68729e8b006dffe8cac74b5bb27f8c9aeca
Tony Miller <mcfiredrill@gmail.com>
2017-03-01 16:15:44 +0900

    make migrations compatible

--- db/migrate/20101108180055_add_foreign_to_leafs.rb
+++ db/migrate/20101108180055_add_foreign_to_leafs.rb
@@ -1,4 +1,4 @@
-class AddForeignToLeafs < ActiveRecord::Migration
+class AddForeignToLeafs < ActiveRecord::Migration[4.2]
   def self.up
     add_column :leafs, :branch_id, :integer
   end
--- db/migrate/20130803064029_add_banner_to_boards.rb
+++ db/migrate/20130803064029_add_banner_to_boards.rb
@@ -1,4 +1,4 @@
-class AddBannerToBoards < ActiveRecord::Migration
+class AddBannerToBoards < ActiveRecord::Migration[4.2]
   def change
     add_attachment :boards, :banner
   end
